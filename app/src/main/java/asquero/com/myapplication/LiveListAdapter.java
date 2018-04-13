2d0a4afff5b6a6366784daa5d8806351f5c7e681
Srajan Gupta <srajaninnov@gmail.com>
2018-04-13 23:02:08 +0530

    Networking and Image loading

--- app/src/main/java/asquero/com/myapplication/LiveListAdapter.java
+++ app/src/main/java/asquero/com/myapplication/LiveListAdapter.java
@@ -1,6 +1,8 @@
 package asquero.com.myapplication;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +11,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.silvestrpredko.dotprogressbar.DotProgressBar;
+import com.github.silvestrpredko.dotprogressbar.DotProgressBarBuilder;
+import com.squareup.picasso.Downloader;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.PicassoProvider;
+import com.squareup.picasso.RequestCreator;
 
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -21,6 +29,10 @@ public class LiveListAdapter extends RecyclerView.Adapter<LiveListAdapter.ViewHo
 
     private List<LiveList>list;
     private Context context;
+
+    //Picasso.Builder builder = new Picasso.Builder(context);
+
+
 
     public LiveListAdapter(List<LiveList> listLive, Live live) {
         this.list = listLive;
@@ -43,14 +55,6 @@ public class LiveListAdapter extends RecyclerView.Adapter<LiveListAdapter.ViewHo
         holder.endDate.setText(""+(listItem.getStartDate()));
         holder.startDate.setText(""+(listItem.getEndDate()));
         //holder.imageView.setImageResource(listItem.getImage());
-        /*try {
-            super(itemView);
-            ImageView imagev = (ImageView) itemView.findViewById(R.id.imageView);
-            Picasso.get().load("http://i.imgur.com/DvpvklR.png").placeholder(R.drawable.upcoming).error(R.drawable.ended).into(imagev);
-        }
-        catch (java.lang.RuntimeException e){
-            //Toast.makeText(getApplicationContext(), "Picasso Failed", Toast.LENGTH_SHORT).show();
-        }*/
         holder.aic.setText(listItem.getAIC());
 
     }
@@ -79,7 +83,10 @@ public class LiveListAdapter extends RecyclerView.Adapter<LiveListAdapter.ViewHo
             //ImageView imagev = (ImageView)itemView.findViewById(R.id.imageView);
             ImageView imagev = (ImageView) itemView.findViewById(R.id.imageView);
             try {
-                Picasso.get().load("https://mikkegoes.com/wp-content/uploads/2017/02/css-code-laptop.jpeg").into(imagev);
+
+                /*imageLoader imageLoader = new imageLoader();
+                imageLoader.execute();*/
+                Picasso.get().load("https://edsurge.imgix.net/uploads/post/image/7747/Kids_coding-1456433921.jpg?auto=compress%2Cformat&w=2000&h=810&fit=crop").into(imagev);
             }
             catch (java.lang.RuntimeException e){
                 //Toast.makeText(getApplicationContext(), "Picasso Failed", Toast.LENGTH_SHORT).show();
@@ -88,4 +95,29 @@ public class LiveListAdapter extends RecyclerView.Adapter<LiveListAdapter.ViewHo
             aic = (TextView)itemView.findViewById(R.id.AICTextView);
         }
     }
+
+    /*private class imageLoader extends AsyncTask<View, Void, RequestCreator> {
+
+        protected RequestCreator doInBackground(View... itemView) {
+            //Picasso.Builder builder = new Picasso.Builder(context);
+
+            RequestCreator requestCreator = Picasso.get().load("https://mikkegoes.com/wp-content/uploads/2017/02/css-code-laptop.jpeg");
+            return requestCreator;
+        }
+
+        protected void onProgressUpdate(View... itemView) {
+
+            DotProgressBarBuilder dpbb = (DotProgressBarBuilder) itemView.findViewById(R.id.dot_progress_bar);
+
+            dpbb.setDotAmount(5).setStartColor(Color.BLACK).setAnimationDirection(DotProgressBar.LEFT_DIRECTION).build();
+
+        }
+
+        protected void onPostExecute(RequestCreator result) {
+
+            ImageView imagev = (ImageView) itemView.findViewById(R.id.imageView);
+            result.into(imagev);
+
+        }
+    }*/
 }
